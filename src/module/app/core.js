3177687165b6daf7d27525619e47bdfe8c53b057
Eddie Dover <ed@eddiedover.dev>
2024-02-05 20:59:02 -0800

    updated testing

---
+++ src/module/app/core.js
@@ -0,0 +1,12 @@
+/**
+ * Adds 1 to a number
+ * @param {number} a - The number to add 1 to
+ * @returns {number} The number plus 1
+ */
+export function addOneOrTwo(a) {
+  // @ts-ignore
+  if (game.user.isGM) {
+    return a + 2;
+  }
+  return a + 1;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,11 +1,26 @@
-// SPDX-FileCopyrightText: 2022 Johannes Loher
-//
-// SPDX-License-Identifier: MIT
+// eslint-disable-next-line no-shadow
+import { jest } from "@jest/globals";
+import { addOneOrTwo } from "../src/module/app/core";
 
 describe("An example test", () => {
-  it("will always succeed", () => {
-    expect(true).toBeTruthy();
+  // eslint-disable-next-line no-undef
+  global.game = {
+    user: {
+      isGM: false,
+    },
+    settings: {
+      get: jest.fn().mockReturnValue(1),
+    },
+  };
+
+  it("will add one to a number", () => {
+    const number = 1;
+    let result = addOneOrTwo(number);
+    expect(result).toBe(2);
+    result = addOneOrTwo(number);
+    // eslint-disable-next-line no-undef
+    global.game.user.isGM = true;
+    result = addOneOrTwo(number);
+    expect(result).toBe(3);
   });
 });
-
-// Add real tests here.
